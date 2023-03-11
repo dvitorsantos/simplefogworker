8599e39c8e70917b69cc67238ff7f61502811ec8
Vitor Santos <vitoralannl@gmail.com>
2023-03-11 08:10:29 -0300

    Initial commit

---
+++ src/main/java/lsdi/fogworker/FogworkerApplication.java
@@ -0,0 +1,13 @@
+package lsdi.fogworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogworkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsdi/fogworker/FogworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package lsdi.fogworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
